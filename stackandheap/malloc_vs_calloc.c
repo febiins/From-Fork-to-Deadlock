27d472e4b03020d3052088160ff65e76e85569ae
febin <febinsunny2004@gmail.com>
2026-05-22 08:07:22 +0530

    malloc vs calloc

---
+++ stackandheap/malloc_vs_calloc.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+
+    int *p1;
+    int *p2;
+
+    p1 = malloc(5 * sizeof(int));
+    p2 = calloc(5, sizeof(int));
+
+    printf("malloc:\n");
+
+    for(int i=0;i<5;i++)
+        printf("%d ", p1[i]);
+
+    printf("\n\ncalloc:\n");
+
+    for(int i=0;i<5;i++)
+        printf("%d ", p2[i]);
+
+    free(p1);
+    free(p2);
+
+    return 0;
+}
